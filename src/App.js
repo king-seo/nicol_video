055eae6d4181073e0ab89c9dade317bbaa2d479f
seo hee Kim <kshradioo@gmail.com>
2019-08-20 14:12:12 +0900

    cat image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+function Cat({ name, picture }) {
+  return (
+    <div>
+      <h1>I love {name}</h1>
+      <img src={picture} />
+    </div>
+  );
+}
 
+const catILike = [
+  {
+    name: "bengal",
+    image:
+      "https://www.thehappycatsite.com/wp-content/uploads/2018/01/bengal-cat-names.jpg"
+  },
+  {
+    name: "abisinian",
+    image:
+      "http://mblogthumb2.phinf.naver.net/20130426_289/udadacat2_1366961321378tH3Ly_JPEG/%BE%C6%BA%F1%BD%C3%B4%CF%BE%C8%B0%ED%BE%E7%C0%CC2.jpg?type=w2"
+  },
+  {
+    name: "munchikin",
+    image: "http://cfile213.uf.daum.net/image/226FB84E53521AEA34816B"
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {catILike.map(obj => (
+        <Cat name={obj.name} picture={obj.image} />
+      ))}
     </div>
   );
 }

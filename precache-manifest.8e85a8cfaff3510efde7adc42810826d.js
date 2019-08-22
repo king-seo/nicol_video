da533080e894e38f1b7d7a7d88b16bd10f1dd83c
seo hee Kim <kshradioo@gmail.com>
2019-08-22 19:49:15 +0900

    Updates

---
+++ precache-manifest.8e85a8cfaff3510efde7adc42810826d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38fe6d804c612d97353d28c7f4b5c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecdc707286d004631067",
+    "url": "/static/css/main.b0bff586.chunk.css"
+  },
+  {
+    "revision": "22b54577c527d33d971d",
+    "url": "/static/js/2.922cb9c9.chunk.js"
+  },
+  {
+    "revision": "ecdc707286d004631067",
+    "url": "/static/js/main.a130ae81.chunk.js"
+  },
+  {
+    "revision": "0b9020bc7c0ce210b75b",
+    "url": "/static/js/runtime~main.12231d8d.js"
+  }
+]);
